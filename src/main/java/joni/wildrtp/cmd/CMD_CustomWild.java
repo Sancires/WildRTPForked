5149d91aedebbb28b13019559909e50a8c38a712
Jonathan Renz <jonathan.renz@kabelmail.de>
2024-10-07 19:37:07 +0200

    version 2.6 messages and cooldown for customrtp new error messages for customrtp

--- src/main/java/joni/wildrtp/cmd/CMD_CustomWild.java
+++ src/main/java/joni/wildrtp/cmd/CMD_CustomWild.java
@@ -5,12 +5,18 @@ import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 
+import joni.utils.ConfigLoader;
+import joni.utils.CooldownManager;
+import joni.wildrtp.WildRTP;
 import joni.wildrtp.api.RandomPoint.Algorithm;
 import joni.wildrtp.api.TeleportToRandom;
 
 public class CMD_CustomWild implements CommandExecutor {
+
+	private static final String prefix = ConfigLoader.loadPrefix();
 
 	@Override
 	public boolean onCommand(CommandSender s, Command cmd, String label, String[] args) {
@@ -18,21 +24,45 @@ public class CMD_CustomWild implements CommandExecutor {
 		if (!s.hasPermission("wildrtp.custom"))
 			return false;
 
-		if (args.length != 6) {
-			s.sendMessage("Use /customrtp <player> <world> <startRadius> <endRadius> <originX> <originZ>");
+		if (!(args.length >= 6 && args.length <= 8)) {
+			s.sendMessage(prefix + "Arguments:");
+			s.sendMessage(prefix + "<required arguments>");
+			s.sendMessage(prefix + "(optional arguments)");
+			s.sendMessage(prefix
+					+ "Use /customrtp <player> <world> <startRadius> <endRadius> <originX> <originZ> (cooldown true / false) (messages true / false)");
 			return false;
 		}
 
 		Player p = Bukkit.getPlayer(args[0]);
 		if (p == null) {
-			s.sendMessage("The player is not online!");
+			s.sendMessage(prefix + "The player is not online!");
 			return false;
 		}
 
 		World w = Bukkit.getWorld(args[1]);
 		if (w == null) {
-			s.sendMessage("The world does not exists!");
+			s.sendMessage(prefix + "The world does not exists!");
 			return false;
+		}
+
+		if (args.length >= 7) {
+			if (Boolean.parseBoolean(args[6])) {
+				FileConfiguration config = WildRTP.getPlugin().getConfig();
+				Boolean cooldown = false;
+				if (config.getBoolean("cooldown.enabled"))
+					cooldown = CooldownManager.isOnCooldwon(p);
+
+				if (cooldown)
+					return false;
+			}
+
+			if (args.length >= 8) {
+				if (Boolean.parseBoolean(args[7])) {
+					TeleportToRandom.teleportWithInfo(w, Algorithm.CIRCLE, Double.parseDouble(args[2]),
+							Double.parseDouble(args[3]), Integer.parseInt(args[4]), Integer.parseInt(args[5]), p);
+					return false;
+				}
+			}
 		}
 
 		TeleportToRandom.teleport(w, Algorithm.CIRCLE, Double.parseDouble(args[2]), Double.parseDouble(args[3]),
